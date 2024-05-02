d7a698343ad9a72269d84b44d33c239af5648d62
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2024-05-02 02:58:23 +0100

    Updated the Group-by Order-by file

--- learnSQL/0x04_GroupBy-OrderBy.sql
+++ learnSQL/0x04_GroupBy-OrderBy.sql
@@ -1,32 +1,71 @@
-SELECT *
-FROM employee_id
-ORDER BY age DESC
---arranges the table in descending order of age
+SELECT 
+    *
+FROM
+    employee_id
+ORDER BY age DESC;
+-- arranges the table in descending order of age
 
-SELECT *
-FROM employee_id
-ORDER BY age, gender 
--- arranges the table in order of age and gender (the default order in SQL is ascending)
+SELECT 
+    *
+FROM
+    employee_id
+ORDER BY age;
+-- arranges the table in order of age (the default order in SQL is ascending)
 
-SELECT *
-FROM employee_id
-ORDER BY age, gender DESC
---arranges the table in descending order of gender (age takes the default ascending order)
+SELECT 
+    *
+FROM
+    employee_id
+ORDER BY gender, age DESC;
+-- arranges the table in Ascending order of gender and descending order of age
 
-SELECT *
-FROM employee_id
-ORDER BY 4 desc
---here, the column name is replaced with number (columns are numbered based on their position)
-id | firstname | lastname | age | gender
-id=1, firstname=2, lastname=3, ...
+SELECT 
+    *
+FROM
+    employee_id
+ORDER BY  age, gender DESC;
+-- arranges the table in Ascending order of age and descending order of gender
+-- (however, the age querry takes prescedence here making the gender useless)
 
-SELECT Gender, COUNT(gender) AS genderCount
-FROM employee_id
-GROUP BY gender
---groups the whole date based on gender and gives the total number of each gender as gnder count column
+SELECT 
+    *
+FROM
+    employee_demographics
+ORDER BY gender DESC , age DESC;
 
-SELECT age, count(age) AS ageCount
-FROM employee_id
-GROUP BY age
---groups the data by age giving the total number of people with each age as ageCount column
+SELECT 
+    *
+FROM
+    employee_id
+ORDER BY 4 DESC;
+-- here, the column name is replaced with number (columns are numbered based on their position)
+-- id | firstname | lastname | age | gender
+-- id=1, firstname=2, lastname=3, ...
 
+-- GROUP BY -- 
+SELECT 
+    Gender, COUNT(gender) AS gender_Count
+FROM
+    employee_id
+GROUP BY gender;
+-- groups the whole date based on gender and gives the total number of each gender as gender_Count column
+
+SELECT 
+    age, COUNT(age) AS ageCount
+FROM
+    employee_id
+GROUP BY age;
+-- groups the data by age giving the total number of people with each age as ageCount column
+
+SELECT 
+    gender, AVG(age)
+FROM
+    employee_demographics
+group by gender;
+-- tells us the average age of the employees by gender
+
+SELECT 
+    gender, MIN(age), MAX(age), COUNT(age), AVG(age)
+FROM
+    employee_demographics
+GROUP BY gender;
