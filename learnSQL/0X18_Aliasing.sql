e55d094679a144b5c2ffaac643604bc5bf1e8a2a
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2024-05-17 00:49:06 +0100

    Updated Aliasing

--- learnSQL/0X18_Aliasing.sql
+++ learnSQL/0X18_Aliasing.sql
@@ -1,1 +1,22 @@
 -- Aliasing
+-- Order of execution
+/*
+Step 1: FROM (aliases are defined here for JOIN)
+Step 2: WHERE
+Step 3: SELECT (aliases are defined here)
+Step 4: LIMIT
+*/
+-- Aliases defined in the SELECT clause cannot be used in the WHERE clause
+-- due to the order of execution
+
+-- For example, the following statement will throw an error
+SELECT 
+    budget AS max_budget
+FROM
+    films
+WHERE
+    max_budget IS NOT NULL;
+# error: column "max_budget" does not exist
+# LINE %: WHERE max_budget IS NOT NUL;
+
+-- 
