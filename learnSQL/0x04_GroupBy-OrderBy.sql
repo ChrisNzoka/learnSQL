c3aeb387b86cffdf9968fb23aff20a4e1dbbe792
Chris Nzoka-Okoye <chrisnzokaokoye@gmail.com>
2024-05-19 03:28:54 +0100

    Updated aAggregate function

--- learnSQL/0x04_GroupBy-OrderBy.sql
+++ learnSQL/0x04_GroupBy-OrderBy.sql
@@ -49,7 +49,23 @@ ORDER BY 4 DESC;
 -- id | firstname | lastname | age | gender
 -- id=1, firstname=2, lastname=3, ...
 
--- GROUP BY -- 
+-- ---------------------------- GROUP BY ------------------------------------------------ -- 
+-- Error handling
+#SQL will return an error if we try to SELECT a field that is not in our GROUP BY clause.
+-- Example
+SELECT 
+    certificate, title
+FROM
+    films
+GROUP BY certificates;
+#We'll need to correct this by adding an aggregate function around title.
+SELECT 
+    certificate, COUNT(title) sum_title
+FROM
+    films
+GROUP BY certificates;
+
+#Further examples
 SELECT 
     Gender, COUNT(gender) AS gender_Count
 FROM
@@ -76,3 +92,87 @@ SELECT
 FROM
     employee_demographics
 GROUP BY gender;
+
+-- GROUP BY with ORDER BY
+# We can combine GROUP BY with ORDER BY to group our results,
+-- make a calculation, and then order our results.
+SELECT 
+    certificate, COUNT(title) title_count
+FROM
+    films
+GROUP BY certificates
+order by title_count;
+-- ORDER BY is always written after GROUP BY,
+-- and notice that we can refer back to the alias within the query.
+-- That is because of the order of execution.
+/*
+Our updated queries will begin with FROM
+followed by grouping
+selecting the data
+creating the alias
+sorting the results
+limiting them to the desired number.
+*/
+
+
+SELECT 
+    release_year, COUNT(*) AS film_count
+FROM
+    films
+GROUP BY release_year;
+
+-- Find the release_year and average duration of films for each year
+SELECT 
+    release_year, AVG(duration) avg_duration
+FROM
+    films
+GROUP BY release_year;
+
+-- Find the release_year, country, and max_budget, then group and order by release_year and country
+SELECT 
+    release_year, country, MAX(budget) AS max_budget
+FROM
+    films
+GROUP BY release_year , country
+ORDER BY release_year , country;
+
+-- Which release_year had the most language diversity
+SELECT 
+    release_year, COUNT(DISTINCT language) lang_count
+FROM
+    films
+GROUP BY release_year
+ORDER BY lang_count DESC;
+
+-- HAVING --
+# In SQL, we can't filter aggregate functions with WHERE clauses.
+# For example, this query attempting to filter the title count is invalid:
+
+SELECT 
+    release_year, COUNT(DISTINCT language) lang_count
+FROM
+    films
+GROUP BY release_year
+WHERE lang_count > 10;
+
+-- Instead of using where after grouping, we'll use having:
+SELECT 
+    release_year, COUNT(DISTINCT language) lang_count
+FROM
+    films
+GROUP BY release_year
+HAVING lang_count > 10;
+
+/*. Order of execution
+The reason why groups have their own keyword for filtering comes down to the order of execution.
+The order of execution is:
+FROM
+WHERE
+GROUP BY
+HAVING
+SELECT
+ORDER BY
+LIMIT.
+By reviewing this order, we can see WHERE is executed before GROUP BY and before any aggregation occurs. 
+This order is also why we cannot use the alias with HAVING, but we can with ORDER BY.
+*/
--- learnSQL/0x07_Having.sql
+++ learnSQL/0x07_Having.sql
@@ -46,3 +46,32 @@ GROUP BY occupation
 HAVING avg_sal > 50000;
 -- This works as the "Where" targets the occupation column
 -- while the "Having" targets the aggregated column
+
+/*. Order of execution
+The reason why groups have their own keyword for filtering comes down to the order of execution.
+The order of execution is:
+FROM
+WHERE
+GROUP BY
+HAVING
+SELECT
+ORDER BY
+LIMIT.
+By reviewing this order, we can see WHERE is executed before GROUP BY and before any aggregation occurs. 
+This order is also why we cannot use the alias with HAVING, but we can with ORDER BY.
+*/
+
+/*
+4. HAVING vs WHERE
+WHERE filters individual records while HAVING filters grouped records. We'll walk through two business questions here to show how to translate them into the correct filter. The first question is "What films were released in the year 2000?". This question does not indicate any sort of grouping. It asks to see only the titles from a specific year and can therefore be written as SELECT title, FROM films, WHERE release year equals 2000.
+The second question is, "In what years was the average film duration over two hours?". Straight away, we can see this question has a few more layers. Let's break down the question and query into smaller, easier-to-understand steps.
+This question requires us to return information about years, so we select the release year from the films table. Next, it asks for the average film duration, which tells us we need to place AVG(duration) somewhere. Since we do not need to provide any additional information around the duration on its own, it is unlikely we need to perform the aggregation within the SELECT clause, so we'll try the HAVING clause instead. The last part of the question indicates we need to filter on the duration. Since we can't filter aggregates with WHERE, this supports our theory about using HAVING!
+Finally, we need to add a GROUP BY into our query since we have selected a column that has not been aggregated. Recall the aggregate function will convert the duration values into one average value. Going back to the start of our question, we're interested in knowing the average duration per year, so we group it by release year.
+*/
+
+SELECT 
+    release_year
+FROM
+    films
+GROUP BY release_year
+HAVING avg(duration) > 10;
